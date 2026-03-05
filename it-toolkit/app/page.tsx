e0f18fec59f0ed7905fe8889a645787b9253517e
Isaac Netti <70241315+IsaacNetti@users.noreply.github.com>
2026-03-05 20:30:21 +0000

    Init

---
+++ it-toolkit/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to The IT toolkit</h1>
+      <p>Select a tool</p>
+    </div>
+  );
+}
